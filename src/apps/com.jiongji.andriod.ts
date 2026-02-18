d6530f93d6a8b4de1afd1a3cbf38b707bfc2fb1d
Ling0402 <cjy081219@Gmail.com>
2026-02-18 17:31:01 +0800

    feat: 通知提示-关闭断电保护卡消耗提醒💡
    Signed-off-by: Ling0402 <cjy081219@Gmail.com>

---
+++ src/apps/com.jiongji.andriod.ts
@@ -0,0 +1,71 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.jiongji.andriod',
+  name: '百词斩1',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: [
+        {
+          fastQuery: true,
+          excludeActivityIds:
+            'com.baicizhan.client.business.webview.ui.BczWebActivity',
+          matches:
+            '[text*="跳过"][text.length<10][width<500 && height<300][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/8612bbae-7a50-4730-8071-1c47e35c27ca',
+          snapshotUrls: 'https://i.gkd.li/i/21705324',
+          excludeSnapshotUrls: 'https://i.gkd.li/i/21705338',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '全屏广告-活动弹窗',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.baicizhan.main.activity.MainTabActivity',
+          matches:
+            '@ImageView[clickable=true][visibleToUser=true] -2 [text="反馈"]',
+          snapshotUrls: 'https://i.gkd.li/i/13415075',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '更新提示',
+      fastQuery: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: 'com.baicizhan.main.activity.MainTabActivity',
+          matches: '[text="抢先体验"] - [text="取消"]',
+          snapshotUrls: 'https://i.gkd.li/i/13488652',
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '通知提示-关闭断电保护卡消耗提醒💡',
+      fastQuery: true,
+      rules: [
+        {
+          activityIds: 'com.baicizhan.main.activity.MainTabActivity',
+          matches: '[vid="a2v"]',
+          snapshotUrls: 'https://i.gkd.li/i/24887224',
+        },
+      ],
+    },
+  ],
+});
